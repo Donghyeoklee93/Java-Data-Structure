8548594fb5d0573ada0d4c3a7021555b5581a857
Donghyeok (Henry) Lee <119444211+Donghyeoklee93@users.noreply.github.com>
2023-02-25 17:53:22 +0000

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,14 @@
+package dataStructure;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+public class Test1 {
+    public static void main(String[] args) {
+        ArrayList obj = new ArrayList();
+        obj.add("Keith");
+        obj.add("Paul");
+        System.out.println(obj);
+    }
+}
+
